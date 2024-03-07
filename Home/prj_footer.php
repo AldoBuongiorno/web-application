b48f7f23a0711217340738437382f10b77dff301
ChristianRosa02 <christianrosa19082002>
2024-03-07 17:14:36 +0100

    footer

--- Home/prj_footer.php
+++ Home/prj_footer.php
@@ -1,31 +1,31 @@
 <link rel="stylesheet" href="../Home/prj_footer.css">
 
-<footer class="footer">
-    <div class="container-footer">
-        <div class="contact">
-            <h5 style="color: #f2f2f2;">CONTATTACI</h5>
+<footer id="footer">
+    <div id="container-footer">
+        <div id="contact">
+            <h5 class="titoli-footer">CONTATTACI</h5>
             <hr class="line">
-            <ul class="list">
+            <ul id="list">
                 <li>C.ROSA10@studenti.unisa.it</li>
                 <li>R.CALABRESE26@studenti.unisa.it</li>
                 <li>S.TARTAGLIONE@studenti.unisa.it</li>
                 <li>A.BUONGIORNO5@studenti.unisa.it</li>
             </ul>
         </div>
-        <div class="who">
-            <h5 style="color: #f2f2f2;">CHI SIAMO</h5>
+        <div id="who">
+            <h5 class="titoli-footer">CHI SIAMO</h5>
             <hr class="line">
-            <ul class="listInsta">
+            <ul id="listInsta">
                 <li><a href="https://www.instagram.com/christian_rosa___?igsh=MTZla3B0YnFvNHdrcQ%3D%3D&utm_source=qr" target="_blank">Christian Rosa</a></li>
                 <li><a href="https://www.instagram.com/raffaele.calabrese_?igsh=bnNpbmlsdHd0N2p0" target="_blank">Raffaele Calabrese</a></li>
                 <li><a href="https://www.instagram.com/salvatore__tartaglione?utm_source=qr&igsh=MXFoNXI4bDQ0bzNwbw==" target="_blank">Salvatore Tartaglione</a></li>
                 <li><a href="https://www.instagram.com/aldo_buongiorno?utm_source=qr" target="_blank">Aldo Buongiorno</a></li>
             </ul>
         </div>
-        <div class="follow">
-            <h5 style="color: #f2f2f2;">SEGUICI ANCHE SU:</h5>
+        <div id="follow">
+            <h5 class="titoli-footer">SEGUICI ANCHE SU:</h5>
             <hr class="line">
-            <ul class="listSocial">
+            <ul id="listSocial">
                 <li><a href="https://www.facebook.com/?locale=it_IT" target="_blank"><img src="../Images/imgHome/icons8-facebook-96.png" alt="facebooklogo" class="social"></a></li>
                 <li><a href="https://www.instagram.com/" target="_blank"><img src="../Images/imgHome/icons8-instagram-96.png" alt="instalogo" class="social"></a></li>
                 <li><a href="https://www.youtube.com/" target="_blank"><img src="../Images/imgHome/icons8-riproduci-youtube-96.png" alt="youtubelogo" class="social"></a></li>
@@ -33,7 +33,7 @@
             </ul>
         </div>
     </div>
-    <div class="copyright">
-        <p style="color: #f2f2f2;">Copyright &copy; 2024. Progettato da <i>Gruppo27</i> a <i>Fisciano</i></p>
+    <div id="copyright">
+        <p class="titoli-footer">Copyright &copy; 2024. Progettato da <i>Gruppo27</i> a <i>Fisciano</i></p>
     </div>
 </footer>
