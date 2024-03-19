7ed89294acb70acb855fb284f891bfbb2d189c0e
SalvatoreFTartaglione <s.tartaglione@studenti.unisa.it>
2024-03-19 15:30:56 +0100

    comments

--- Summary/prj_summary.php
+++ Summary/prj_summary.php
@@ -206,7 +206,6 @@
         </div>
 
         <div class="slideshows-container">
-            <!-- INSERIRE CAPTION E NUMBER TEXT IN MANIERA DINAMICA -->
             <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
             <a class="next" onclick="plusSlides(1)">&#10095;</a>
         </div>
